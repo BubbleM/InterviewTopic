b32dece184a5719edb13fda91a038d51d0712e31
BubbleM <1870408473@qq.com>
2017-08-08 14:15:21 +0800

    add 08-08 TWTask

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ JS面试题目练习集
 
 ## DOM练习
 1. [获取页面中所有元素标签中出现次数最多的标签](https://github.com/BubbleM/InterviewTopic/blob/master/DOM/practice1.js)
+
+
+
+## TW体能大比拼
+1. [题目：将一个正整数分解质因数。例如：输入90,打印出90=2335]()
---
+++ TW-tasks/practice1.js
@@ -0,0 +1,26 @@
+/**
+ * Created by bubble on 17-8-8.
+ */
+function integerToPrime(num) {
+  var primeArr = [];
+  for (let i = 2; i <= num; i++) {
+    while (i != num) {
+      if (num % i === 0) {
+        primeArr.push(i);
+        num = num / i;
+      } else {
+        break;
+      }
+    }
+  }
+  primeArr.push(num);
+  return primeArr;
+}
+
+function printPrime(num) {
+  let primeArr = integerToPrime(num);
+  let str = num + '=' + primeArr.join('');
+  console.log(str);
+}
+
+printPrime(90);
