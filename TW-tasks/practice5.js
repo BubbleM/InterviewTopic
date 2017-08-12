f326be5dd4d8ad8277dd4a8c79513dbedeaad0d3
BuddleM <1870408473@qq.com>
2017-08-12 14:17:40 +0800

    add practice5

---
+++ TW-tasks/practice5.js
@@ -0,0 +1,19 @@
+function getCubeAdd (num) {
+	let total = 0;
+	let string = num.toString();
+	for (let i = 0; i < string.length; i++) {
+		total += Math.pow(string.charAt(i) - 0, 3);
+	}
+	
+	return total;
+}
+
+ function printNarcissus () {
+ 	for (let i = 100; i < 1000; i++) {
+ 		if (getCubeAdd(i) === i) {
+ 			console.log(i);
+ 		}
+ 	}
+ }
+
+printNarcissus();
