6c948fed8ddce86044262f6e873a49231447fe8e
BubbleM <1870408473@qq.com>
2017-08-13 16:15:00 +0800

    update practice6

--- TW-tasks/practice6.js
+++ TW-tasks/practice6.js
@@ -1,22 +1,15 @@
-/**
- * Created by bubble on 17-8-13.
- */
 function findMiddleNum(arr) {
-  let middleNum;
   if (arr.length % 2 === 0) {
-    middleNum = (arr[parseInt(arr.length / 2)] + arr[parseInt(arr.length / 2) -  1]) / 2;
-  } else {
-    middleNum =  arr[parseInt(arr.length / 2)];
+    return (arr[arr.length / 2] + arr[arr.length / 2 -  1]) / 2;
   }
-  return middleNum;
+
+  return arr[parseInt(arr.length / 2)];
 }
 
 function printMiddleNum(arr) {
-  arr.sort((a, b) => {
-    return a - b;
-  });
+  arr.sort((a, b) => a - b);
   console.log(findMiddleNum(arr));
 }
 
-let arr = [1,3,2,5];
+let arr = [1, 3, 2, 5];
 printMiddleNum(arr);
