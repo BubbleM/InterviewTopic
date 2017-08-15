3c82c9117c9108a38094ba3620cbfb17d6d8c6eb
BubbleM <1870408473@qq.com>
2017-08-15 15:17:15 +0800

    add practice7

---
+++ TW-tasks/practice7.js
@@ -0,0 +1,11 @@
+function numCount(arr, num) {
+    let count = 0;
+    arr.forEach(item => {
+        if (item === num) {
+            count++;
+        }
+    });
+
+    return count;
+}
+console.log(numCount([1, 3, 7, 1 ,4], 1));
