3916ca81b1840e691e634c8a2e5a89dfdb9428c0
BubbleM <1870408473@qq.com>
2017-08-15 15:27:03 +0800

    update practice7

--- TW-tasks/practice7.js
+++ TW-tasks/practice7.js
@@ -1,3 +1,11 @@
-/**
- * Created by bubble on 17-8-15.
- */
+function numCount(arr, num) {
+    let count = 0;
+    arr.forEach(item => {
+        if (item === num) {
+            count++;
+        }
+    });
+
+    return count;
+}
+numCount([1, 3, 7, 1 ,4], 1);
