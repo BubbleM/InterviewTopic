3f4fc80a46e7e1290b0fe228d4a015768795459b
BubbleM <1870408473@qq.com>
2017-08-21 16:22:19 +0800

    add reverse Array

--- JS/practice12.js
+++ JS/practice12.js
@@ -1,3 +1,11 @@
-/**
- * Created by bubble on 17-8-21.
- */
+function myReverse(arr) {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let temp = arr[i];
+    arr[i] = arr[arr.length - i - 1];
+    arr[arr.length - i - 1] = temp;
+  }
+  return arr;
+}
+
+let arr = [1, 2, 3, 4];
+console.log(myReverse(arr));
