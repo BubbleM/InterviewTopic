928737801166109d70fbea79d43ecaa83232bea9
BubbleM <1870408473@qq.com>
2017-08-21 15:43:32 +0800

    update practice8

--- JS/practice8.js
+++ JS/practice8.js
@@ -1,16 +1,3 @@
-Math.random(); // 返回0~1之间的小数　不包含0和1
-Math.floor(Math.random() * 10 + 1); // 返回1到10之间的整数
-
-function rd(n, m) { // 返回n-m之间的数
-  let c = m - n + 1;
-  return Math.floor(Math.random() * c + n);
-}
-var n = 5;
-var m = 100;
-console.log(rd(n, m));
-
-console.log(Math.floor(Math.random() * 100)); // 0 ~ 99 之间的数
-
 function createArr(num) {
   let arr = [];
   for (let i = 0; i <= num; i++) {
@@ -25,8 +12,8 @@ function getRandom(num) {
   for (let i = 0; i < num; i++) {
     let index = Math.floor(Math.random()*100); /*每次随机取出一个数*/
     if (baseArr[index]) {
-      result.push(baseArr[index]);
-      baseArr[index] = undefined;　/*去除数组中取出的这个数,实现永不重复*/
+      result.push(baseArr.splice(index, 1));
+      // baseArr[index] = undefined;　/*去除数组中取出的这个数,实现永不重复*/
     } else {
       num += 1;
     }
