66f5d0ca76100b33cffcb23636548ca60ba51094
BubbleM <1870408473@qq.com>
2017-08-22 14:28:12 +0800

    Update practice13.js

--- TW-tasks/practice13.js
+++ TW-tasks/practice13.js
@@ -1,14 +1,14 @@
 function intersect(arr1, arr2){
-	let result = [];
-	arr1.forEach(item => {
-		if (arr2.includes(item)) {
-			let index = arr2.indexOf(item);
-			result.push(item);
-			arr2[index] = '';
-		}
-	});
+  let result = [];
+  arr1.forEach(item => {
+    if (arr2.includes(item)) {
+      let index = arr2.indexOf(item);
+      result.push(item);
+      arr2[index] = '';
+    }
+  });
 
-	return result;
+  return result;
 }
 let nums1 = [1, 2, 2, 1, 2];
 let nums2 = [2, 2];
