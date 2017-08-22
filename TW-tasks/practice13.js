5298fefcf40a8c0dcb9dbf5bd2f58d0bd9ff3469
BubbleM <1870408473@qq.com>
2017-08-22 14:27:11 +0800

    Create practice13.js

---
+++ TW-tasks/practice13.js
@@ -0,0 +1,15 @@
+function intersect(arr1, arr2){
+	let result = [];
+	arr1.forEach(item => {
+		if (arr2.includes(item)) {
+			let index = arr2.indexOf(item);
+			result.push(item);
+			arr2[index] = '';
+		}
+	});
+
+	return result;
+}
+let nums1 = [1, 2, 2, 1, 2];
+let nums2 = [2, 2];
+console.log(intersect(nums1, nums2));
