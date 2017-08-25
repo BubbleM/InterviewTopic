a759a6fe3f087f464638758a1f17c6dbe5d3dd48
BubbleM <1870408473@qq.com>
2017-08-25 15:59:18 +0800

    update

--- TW-tasks/practice16.js
+++ TW-tasks/practice16.js
@@ -28,10 +28,3 @@ function formatArr(nums, target) {
 let nums = [0, 1, 0, 3, 12];
 let target = 0;
 console.log(formatArr(nums, target));
-
-
-/*
-我最近两天经常遇到一个问题，场景是这样的：
-  我需要不断的添加数据，所以要将这些数据保存在一个全局变量中。
-  就像刚才的nums。我需要多次对num进行操作，所以我创建了一个main()方法将构建一个作用域。
-  但是在实际操作中，我其实*/
