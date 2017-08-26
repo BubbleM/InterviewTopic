af509e2fc34805532a995fff090c593239a24719
BubbleM <1870408473@qq.com>
2017-08-26 15:08:04 +0800

    add .

--- TW-tasks/practice17.js
+++ TW-tasks/practice17.js
@@ -13,7 +13,7 @@ function fizzBuzz(num) {
 function printFizzBuzz(num) {
   for (let i = 1; i <= num; i++) {
     if (i % 3 === 0 || i % 5 === 0)
-      console.log(i + ':' + fizzBuzz(i));
+      console.log(i + ':' + fizzBuzz(i) + '.');
   }
 }
 
